7460848b7e52c6225d1d41d16157435417cc43b2
Amid <96419691+AmiD4567@users.noreply.github.com>
2026-08-24 04:30:37 +1000

    Delete chat-app directory

--- chat-app/electron/badge-preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Preload специально для offscreen окна создания бейджа
-contextBridge.exposeInMainWorld('badgeAPI', {
-  sendResult: (dataURL) => ipcRenderer.send('badge-result', dataURL),
-  sendOverlayResult: (dataURL) => ipcRenderer.send('badge-overlay-result', dataURL)
-});
